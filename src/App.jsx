34bf89bfaad72a6c4dcff0aaebf7e5878377311a
Haroon <you@example.com>
2024-08-08 10:01:29 +0300

    feat: Add Button3 component with CSS-in-JS styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import './App.css';
 import Button1 from './components/example-1-normal-css/Button';
 import Button2 from './components/example-2-inline-css/Button';
+import Button3 from './components/example-3-css-in-js/Button';
 const App = () => {
   return (
     <div className="App">
       <Button1 buttonText="Example 1" />
       <Button2 buttonText="Example 2" />
+      <Button3 buttonText="Example 3" />
     </div>
     
   );
